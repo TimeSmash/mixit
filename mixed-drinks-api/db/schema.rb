6219dcc3caa7ad0753a71b520a81358c1a227be8
TimeSmash <mcumm64@gmail.com>
2019-12-11 23:40:46 -0500

    Added timestamps to UserDrink model so most recent FMI can be selected. Used Dec 1st 2019 as default date (see comments in 20191212942510_add_timestamps... for details)

--- mixed-drinks-api/db/schema.rb
+++ mixed-drinks-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_023654) do
+ActiveRecord::Schema.define(version: 2019_12_12_042510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2019_11_07_023654) do
     t.boolean "reviewed"
     t.string "review_content"
     t.integer "review_stars"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["drink_id"], name: "index_user_drinks_on_drink_id"
     t.index ["user_id"], name: "index_user_drinks_on_user_id"
   end
